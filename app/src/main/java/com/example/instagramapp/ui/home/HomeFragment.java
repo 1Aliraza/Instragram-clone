2c777539f021865123c1f25a7a98ce31eb8d688a
92340 <malikaliraza3749@gmail.com>
2021-08-04 10:06:30 +0500

    first commit

---
+++ app/src/main/java/com/example/instagramapp/ui/home/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.instagramapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.instagramapp.MainActivity;
+import com.example.instagramapp.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    ImageView heart;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return root;
+    }
+}
